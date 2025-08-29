d84105cd5fac22b64285c76e1caaa5392029b3e1
SheharaF01 <shehara.oddly@gmail.com>
2025-08-29 12:10:44 +0530

    Updated cards.tsx

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,70 +1,70 @@
-import React from "react";
-import CardType1 from "./CardType1";
-import CardType2 from "./CardType2";
-import CardType3 from "./CardType3";
-import CardType4 from "./CardType4";
-import CardType5 from "./CardType5";
-import CardType6 from "./CardType6";
-import CardType7 from "./CardType7";
-import CardType7Loading from "./CardType7Loading";
-import CardType8 from "./CardType8";
-import CardType8Loading from "./CardType8Loading";
+// import React from "react";
+// import CardType1 from "./CardType1";
+// import CardType2 from "./CardType2";
+// import CardType3 from "./CardType3";
+// import CardType4 from "./CardType4";
+// import CardType5 from "./CardType5";
+// import CardType6 from "./CardType6";
+// import CardType7 from "./CardType7";
+// import CardType7Loading from "./CardType7Loading";
+// import CardType8 from "./CardType8";
+// import CardType8Loading from "./CardType8Loading";
 
-const Cards: React.FC = () => (
-  <div className="bg-gray-400">
-    <h1 className="text-3xl font-bold text-black text-center py-10 mb-14 border-b border-b-black">
-      Cards
-    </h1>
+// const Cards: React.FC = () => (
+//   <div className="bg-gray-400">
+//     <h1 className="text-3xl font-bold text-black text-center py-10 mb-14 border-b border-b-black">
+//       Cards
+//     </h1>
 
-    <div className="mx-auto max-w-7xl px-6 pt-3 pb-6">
-      <div className="grid grid-cols-1 gap-4">
-        <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
-          Component Cards <br /> Card-type - 1
-        </h3>
-        <CardType1 />
-        <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
-          Component Cards <br /> Card-type - 2
-        </h3>
-        <CardType2 />
-        <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
-          Component Cards <br /> Card-type - 3
-        </h3>
-        <CardType3 />
-        <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
-          Component Cards <br /> Card-type - 4
-        </h3>
-        <CardType4 />
-        <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
-          Component Cards Card-type - 5 (Insight Card)
-        </h3>
-        <CardType5 />
-        <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
-          Component Cards Card-type - 6 (Datasets Card)
-        </h3>
-        <CardType6 />
-        <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
-          Component Cards Card-type - 7 (Download Card icon)
-        </h3>
-        <CardType7 />
-        <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
-          Component Cards Card-type - 7 (Download Card with loading icon)
-        </h3>
-        <CardType7Loading />
-      </div>
+//     <div className="mx-auto max-w-7xl px-6 pt-3 pb-6">
+//       <div className="grid grid-cols-1 gap-4">
+//         <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
+//           Component Cards <br /> Card-type - 1
+//         </h3>
+//         <CardType1 />
+//         <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
+//           Component Cards <br /> Card-type - 2
+//         </h3>
+//         <CardType2 />
+//         <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
+//           Component Cards <br /> Card-type - 3
+//         </h3>
+//         <CardType3 />
+//         <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
+//           Component Cards <br /> Card-type - 4
+//         </h3>
+//         <CardType4 />
+//         <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
+//           Component Cards Card-type - 5 (Insight Card)
+//         </h3>
+//         <CardType5 />
+//         <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
+//           Component Cards Card-type - 6 (Datasets Card)
+//         </h3>
+//         <CardType6 />
+//         <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
+//           Component Cards Card-type - 7 (Download Card icon)
+//         </h3>
+//         <CardType7 />
+//         <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
+//           Component Cards Card-type - 7 (Download Card with loading icon)
+//         </h3>
+//         <CardType7Loading />
+//       </div>
 
-      {/* Second grid container (Type 8 variants) */}
-      <div className="grid grid-cols-1 gap-4 my-20">
-        <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
-          Component Cards Card-type - 8 (Download Large Card)
-        </h3>
-        <CardType8 />
-        <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
-          Component Cards Card-type - 8 (Download Large Card with loading icon)
-        </h3>
-        <CardType8Loading />
-      </div>
-    </div>
-  </div>
-);
+//       {/* Second grid container (Type 8 variants) */}
+//       <div className="grid grid-cols-1 gap-4 my-20">
+//         <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
+//           Component Cards Card-type - 8 (Download Large Card)
+//         </h3>
+//         <CardType8 />
+//         <h3 className="text-white text-xl text-center border-b mb-6 pb-3 border-white">
+//           Component Cards Card-type - 8 (Download Large Card with loading icon)
+//         </h3>
+//         <CardType8Loading />
+//       </div>
+//     </div>
+//   </div>
+// );
 
-export default Cards;
+// export default Cards;
